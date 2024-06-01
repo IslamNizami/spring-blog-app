4c911639249aff9597531f2f36b7f90f605ff854
IslamNizami <islamnizami0046@gmail.com>
2024-06-01 15:48:00 +0400

    article image ready

--- src/main/java/itbrains/az/blogpage2/controller/DashboardController.java
+++ src/main/java/itbrains/az/blogpage2/controller/DashboardController.java
@@ -24,8 +24,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class DashboardController {
@@ -39,6 +45,8 @@ public class DashboardController {
     private UserService userService;
     @Autowired
     private RoleService roleService;
+
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/static/uploads";
 
     @GetMapping("/admin")
     public String index()
@@ -84,8 +92,14 @@ public class DashboardController {
     }
 
     @PostMapping("/admin/article/create")
-    public String articleCreate(@ModelAttribute ArticleCreateDto articleCreateDto)
-    {
+    public String articleCreate(@ModelAttribute ArticleCreateDto articleCreateDto, @RequestParam("image")MultipartFile image) throws IOException {
+        UUID rand = UUID.randomUUID();
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY,rand+image.getOriginalFilename());
+        fileNames.append(image.getOriginalFilename());
+        Files.write(fileNameAndPath,image.getBytes());
+
+        articleCreateDto.setPhotoUrl(rand+image.getOriginalFilename());
         articleService.addArticle(articleCreateDto);
         return "redirect:/admin/article";
     }
